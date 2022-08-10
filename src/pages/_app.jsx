40cca6b0fc68abced9000982f356910439c22409
tocchi <101105704+tocchi-739@users.noreply.github.com>
2022-08-10 21:37:48 +0900

    ✨ mantineの導入

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,16 @@
 import "src/styles/globals.css";
+import { MantineProvider } from "@mantine/core";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{ colorScheme: "light" }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 };
 
 export default MyApp;
