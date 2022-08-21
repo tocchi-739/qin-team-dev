1b6d815ec08418c68f2c28e1de135287fa97b4ba
tocchi <101105704+tocchi-739@users.noreply.github.com>
2022-08-21 13:58:33 +0900

    💄 微修正

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
 
   return (
     <header
-      className={`fixed left-0 z-10 h-[65px] w-full bg-white pb-12 pt-[44px] ${
+      className={`fixed left-0 z-10 h-[65px] w-full bg-white pb-16 pt-[72px] md:pb-12 md:pt-[44px] ${
         dark ? "bg-[#1A1B1E]" : ""
       }`}
     >
@@ -62,7 +62,7 @@ const Header = () => {
               opened={opened}
               onClick={() => setOpened((o) => !o)}
               title={title}
-              color={`${opened ? "#FFFFFF" : ""}`}
+              color={opened ? "#FFFFFF" : ""}
             />
           </div>
 
