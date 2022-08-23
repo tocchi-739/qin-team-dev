910adf4a6e365ad6b18bf728e0361490555dcec7
tocchi <101105704+tocchi-739@users.noreply.github.com>
2022-08-23 14:52:42 +0900

    🐛 apikeyの名前の修正

--- src/libs/client.jsx
+++ src/libs/client.jsx
@@ -3,5 +3,5 @@ import { createClient } from "microcms-js-sdk"; //ES6
 // Initialize Client SDK.
 export const client = createClient({
   serviceDomain: "qin-team-dev-blog", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: process.env.NEXT_PUBLIC_BLOG_API_KEY,
+  apiKey: process.env.BLOG_API_KEY,
 });
