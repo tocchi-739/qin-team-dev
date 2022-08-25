afe1150e56291fc7f2583b56adfa902dbc8ac0c7
tocchi <101105704+tocchi-739@users.noreply.github.com>
2022-08-25 11:36:16 +0900

    💄 ボタンのダークテーマ対応

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,17 @@
+import { useMantineColorScheme } from "@mantine/core";
 import Link from "next/link";
 
 const Button = (props) => {
+  const { colorScheme } = useMantineColorScheme();
+  const dark = colorScheme === "dark";
   return (
     <div className="mt-6 flex items-center justify-center md:mt-10">
       <Link href={props.href}>
-        <a className="inline-block rounded-full bg-gray-800 px-[22px] py-[10px] text-center text-white hover:opacity-60">
+        <a
+          className={`inline-block rounded-full px-[22px] py-[10px] text-center text-white hover:opacity-60 ${
+            dark ? "bg-[#676767]" : "bg-gray-800"
+          }`}
+        >
           {props.text}
         </a>
       </Link>
