9e0de7df9448100fb3c4e367cf913b26dc7d2689
tocchi <101105704+tocchi-739@users.noreply.github.com>
2022-09-11 13:02:41 +0900

    ✨ ツイートへのリンクを追加

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
+import Link from "next/link";
 import Button from "./Button";
 import HomeContentTitle from "./HomeContentTitle";
 
@@ -9,26 +10,33 @@ const Tweet = (props) => {
       <HomeContentTitle title="Twitter" />
       {props.tweetsData.map((tweet) => {
         return (
-          <div className="mt-10 mb-[44px] flex md:min-h-[260px]" key={tweet.id}>
-            <div className="mr-4">
-              <img
-                src={user.profile_image_url}
-                alt="twitterアカウント画像"
-                className="rounded-full"
-              />
-            </div>
-            <div className="w-[80%]">
-              <div className="flex flex-col md:flex-row md:items-center">
-                <h2 className="font-bold">{user.name}</h2>
-                <p className="text-xs font-bold text-[#909296] md:ml-2">
-                  {`@${user.username}・${dayjs(tweet.created_at).format(
-                    "M月D日"
-                  )}`}
-                </p>
+          <Link href={`https://twitter.com/uolYUd2kPpw3yRY/status/${tweet.id}`}>
+            <a
+              className="mt-10 mb-[44px] flex transition duration-200 hover:opacity-60 md:min-h-[260px] "
+              key={tweet.id}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="mr-4">
+                <img
+                  src={user.profile_image_url}
+                  alt="twitterアカウント画像"
+                  className="rounded-full"
+                />
               </div>
-              <p className="mt-1 leading-6">{tweet.text}</p>
-            </div>
-          </div>
+              <div className="w-[80%]">
+                <div className="flex flex-col md:flex-row md:items-center">
+                  <h2 className="font-bold">{user.name}</h2>
+                  <p className="text-xs font-bold text-[#909296] md:ml-2">
+                    {`@${user.username}・${dayjs(tweet.created_at).format(
+                      "M月D日"
+                    )}`}
+                  </p>
+                </div>
+                <p className="mt-1 leading-6">{tweet.text}</p>
+              </div>
+            </a>
+          </Link>
         );
       })}
       <Button
