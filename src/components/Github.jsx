0b4c21bb8600fc65cd439b6a8bb1125dec270467
tocchi <101105704+tocchi-739@users.noreply.github.com>
2022-10-08 16:40:15 +0900

    issue #30

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -10,86 +10,85 @@ const Github = (props) => {
       {props.githubList.map((github) => {
         return (
           <div className="mt-8 md:mt-10" key={github.id}>
-            <Link href={github.url}>
-              <a
-                className="transition duration-200 hover:opacity-60"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <h3 className="text-lg font-bold">{github.name}</h3>
-                <p className="mt-2">{github.description}</p>
-                <ul className="mt-2 flex font-bold text-gray-500">
-                  <li className="mr-4 flex">
-                    <Image
-                      src={"/assets/svgs/Star.svg"}
-                      width={25}
-                      height={25}
-                      alt="Starのマーク"
-                    />
-                    <p className="ml-1">{github.stargazers.totalCount}</p>
-                  </li>
-                  <li className="mr-1 flex">
-                    <Image
-                      src={"/assets/svgs/Fork.svg"}
-                      width={25}
-                      height={25}
-                      alt="Forkのマーク"
-                    />
-                    <p className="ml-1">{github.forks.totalCount}</p>
-                  </li>
-                </ul>
-                <div className="mt-2">
-                  <span className="flex h-2 w-full overflow-hidden rounded-full">
-                    {github.languages.edges.map((language) => {
-                      return (
+            <a
+              href={github.url}
+              className="transition duration-200 hover:opacity-60"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3 className="text-lg font-bold">{github.name}</h3>
+              <p className="mt-2">{github.description}</p>
+              <ul className="mt-2 flex font-bold text-gray-500">
+                <li className="mr-4 flex">
+                  <Image
+                    src={"/assets/svgs/Star.svg"}
+                    width={25}
+                    height={25}
+                    alt="Starのマーク"
+                  />
+                  <p className="ml-1">{github.stargazers.totalCount}</p>
+                </li>
+                <li className="mr-1 flex">
+                  <Image
+                    src={"/assets/svgs/Fork.svg"}
+                    width={25}
+                    height={25}
+                    alt="Forkのマーク"
+                  />
+                  <p className="ml-1">{github.forks.totalCount}</p>
+                </li>
+              </ul>
+              <div className="mt-2">
+                <span className="flex h-2 w-full overflow-hidden rounded-full">
+                  {github.languages.edges.map((language) => {
+                    return (
+                      <span
+                        style={{
+                          width:
+                            Math.round(
+                              (language.size / github.languages.totalSize) *
+                                1000
+                            ) /
+                              10 +
+                            "%",
+                          background: language.node.color,
+                        }}
+                        key={language.node.id}
+                      ></span>
+                    );
+                  })}
+                </span>
+              </div>
+              <div className="mt-2">
+                <ul className="flex flex-wrap">
+                  {github.languages.edges.map((language) => {
+                    return (
+                      <li
+                        className="mr-4 mt-1 flex items-center"
+                        key={language.node.id}
+                      >
                         <span
-                          style={{
-                            width:
-                              Math.round(
-                                (language.size / github.languages.totalSize) *
-                                  1000
-                              ) /
-                                10 +
-                              "%",
-                            background: language.node.color,
-                          }}
-                          key={language.node.id}
+                          className="h-2 w-2 rounded-full"
+                          style={{ background: language.node.color }}
                         ></span>
-                      );
-                    })}
-                  </span>
-                </div>
-                <div className="mt-2">
-                  <ul className="flex flex-wrap">
-                    {github.languages.edges.map((language) => {
-                      return (
-                        <li
-                          className="mr-4 mt-1 flex items-center"
-                          key={language.node.id}
-                        >
-                          <span
-                            className="h-2 w-2 rounded-full"
-                            style={{ background: language.node.color }}
-                          ></span>
-                          <span className="ml-[6px] text-xs font-bold">
-                            {language.node.name}
-                          </span>
-                          <span className="ml-[6px] text-xs font-bold opacity-70">
-                            {`${
-                              Math.round(
-                                (language.size / github.languages.totalSize) *
-                                  1000
-                              ) / 10
-                            }
+                        <span className="ml-[6px] text-xs font-bold">
+                          {language.node.name}
+                        </span>
+                        <span className="ml-[6px] text-xs font-bold opacity-70">
+                          {`${
+                            Math.round(
+                              (language.size / github.languages.totalSize) *
+                                1000
+                            ) / 10
+                          }
                             %`}
-                          </span>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </div>
-              </a>
-            </Link>
+                        </span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </a>
           </div>
         );
       })}
