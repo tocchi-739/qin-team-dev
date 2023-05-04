b5a0fba5fa6a3a1b1160b4a65e16ff3c8f5485ce
tocchi <101105704+tocchi-739@users.noreply.github.com>
2023-05-04 22:28:54 +0900

    twitter関連の削除・github関連のコメントアウト

--- src/libs/twitterClient.jsx
+++
@@ -1,3 +0,0 @@
-import { TwitterApi } from "twitter-api-v2";
-
-export const twitterClient = new TwitterApi(process.env.TWITTER_USER_TOKEN);
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,10 @@ import Button from "src/components/Button";
 import Github from "src/components/Github";
 import Hero from "src/components/Hero";
 import Portfolio from "src/components/Portfolio";
-import Tweet from "src/components/Tweet";
 import useWindowSize from "src/hooks/useWindowSize";
 import Layout from "src/components/Layout/Layout";
 
 import { client } from "src/libs/client";
-import { twitterClient } from "src/libs/twitterClient";
 const { graphql } = require("@octokit/graphql");
 
 export const getStaticProps = async () => {
@@ -20,31 +18,6 @@ export const getStaticProps = async () => {
   const portfolioData = await client.getList({
     endpoint: "portfolio",
   });
-
-  //Twitterデータ取得
-  // const tweets = await twitterClient.v2.get(
-  //   `users/${process.env.TWITTER_USER_ID}/tweets`,
-  //   {
-  //     max_results: 10,
-  //     "tweet.fields": ["created_at"],
-  //     exclude: ["retweets", "replies"],
-  //     expansions: ["author_id"], //userの情報を取得するための指定
-  //     "user.fields": ["profile_image_url"],
-  //   }
-  // );
-  // const embedTweets = await Promise.all(
-  //   tweets.data.map(async (tweet) => {
-  //     const username = tweets.includes.users[0].username;
-  //     const { id } = tweet;
-  //     const url = `https://publish.twitter.com/oembed?url=https://twitter.com/${username}/status/${id}`;
-  //     const data = await fetch(url);
-  //     const { html } = await data.json();
-  //     if (typeof html !== "string") {
-  //       return { ...tweet, html: "" };
-  //     }
-  //     return { ...tweet, html };
-  //   })
-  // );
 
   //GitHubデータ取得
   const graphqlWithAuth = graphql.defaults({
@@ -86,18 +59,15 @@ export const getStaticProps = async () => {
     }
   }
 `;
-  const { user } = await graphqlWithAuth(QUERY, {
-    login: "tocchi-739",
-  });
+  // const { user } = await graphqlWithAuth(QUERY, {
+  //   login: "tocchi-739",
+  // });
 
   return {
     props: {
       blog: blogData,
       portfolio: portfolioData,
-      twitterUser: tweets.includes.users[0],
-      tweets: tweets,
-      embedTweets: embedTweets,
-      github: user,
+      // github: user,
     },
     revalidate: 10,
   };
@@ -105,10 +75,10 @@ export const getStaticProps = async () => {
 
 const Home = (props) => {
   const width = useWindowSize();
-  const githubUser = {
-    name: props.github.name,
-    url: props.github.url,
-  };
+  // const githubUser = {
+  //   name: props.github.name,
+  //   url: props.github.url,
+  // };
   return (
     <Layout title={"Home"}>
       <Hero />
@@ -129,7 +99,7 @@ const Home = (props) => {
         <Button text="View All" href="/portfolioPage" />
       </div>
       <div className="md:grid md:grid-cols-2 md:gap-20">
-        {width < 640 ? (
+        {/* {width < 640 ? (
           <Github
             githubList={props.github.repositories.nodes.slice(0, 3)}
             githubUser={githubUser}
@@ -139,13 +109,7 @@ const Home = (props) => {
             githubList={props.github.repositories.nodes}
             githubUser={githubUser}
           />
-        )}
-        {/* <Tweet
-          twitterUser={props.twitterUser}
-          embedTweets={
-            width < 640 ? props.embedTweets.slice(0, 3) : props.embedTweets
-          }
-        /> */}
+        )} */}
       </div>
     </Layout>
   );
